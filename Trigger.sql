84edd6880f3abede7431f7ea255ea5b8a71dccc1
Katrina <106999606+katrinamsem@users.noreply.github.com>
2022-06-20 20:52:52 +0100

    Update Trigger.sql

--- Trigger.sql
+++ Trigger.sql
@@ -1,3 +1,6 @@
+-- CREATE TRIGGER 
+-- Minimum deposit that can be inserted into the table is 100, an 'error message' will show when trying to insert an amount less than 100. 
+
 delimiter $$
 CREATE TRIGGER  check_deposit  BEFORE INSERT ON event_finance
 FOR EACH ROW
@@ -8,3 +11,9 @@ SET MESSAGE_TEXT = 'ERROR: DEPOSIT MUST BE AT LEAST 100';
 END IF;
 END; $$
 delimiter; 
+
+-- Demonstrate how it runs - trying to insert the following data will trigger the error message.
+
+Insert into event_finance (event_id, deposit, stock_cost, takings)
+values (408, 99, 575, 900); 
+
